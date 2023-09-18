e77ebd02b992b5f3a8dc7086f870337b757adeb4
Maksim Korelskii <mkorelskii@gmail.com>
2023-09-18 22:43:15 +0300

    refactoring PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   def index
     # render 'pages/index' Это отрабатывает по соглашениям
     # Если передать в строке запроса параметр http://localhost:3000/?username=Maks
-    params[:username].nil? ? @username = 'Guest' : @username = params[:username]
+    # params[:username].nil? ? @username = 'Guest' : @username = params[:username]
+    if current_user
+      @username = current_user&.email.split('@')[0].capitalize
+    else
+      @username = 'Guest'
+    end
   end
 end
