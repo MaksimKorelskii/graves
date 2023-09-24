ce6b6cef5eba189db643a5a7d19516e1d4a3ea09
Maksim Korelskii <mkorelskii@gmail.com>
2023-09-24 15:14:30 +0300

    test models

--- spec/models/cemetery_spec.rb
+++ spec/models/cemetery_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Cemetery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # without gem 'shoulda-matchers'
+  # it 'validates presence of title' do
+  #   cemetery = Cemetery.new(description: 'some description')
+
+  #   expect(cemetery.valid?).to be_falsey
+  #   expect(cemetery).to_not be_valid
+  # end
+
+  it { should belong_to :user }
+  it { should have_many(:graves).dependent(:destroy) }
+
+  it { should validate_presence_of :title }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:cemeteries).dependent(:destroy) }
+  it { should have_many(:graves).dependent(:destroy) }
+
+  # describe "author?" do
+  #   let(:author) { create(:user) }
+  #   let(:user) { create(:user) }
+  #   let(:question) { create(:question, author: author)}
+  #   let(:answer) { create(:answer, author: author, question: question) }
+
+  #   it "is expected user is the author of resource" do
+  #     expect(author.author?(answer)).to be true
+  #     expect(author.author?(question)).to be true
+  #   end
+
+  #   it "is expected user isn't the author of resource" do
+  #     expect(user.author?(answer)).to be false
+  #     expect(user.author?(question)).to be false
+  #   end
+  # end
 end
