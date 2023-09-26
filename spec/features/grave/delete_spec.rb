47671798736894588f6aef7889c249f3b9068318
Maksim Korelskii <mkorelskii@gmail.com>
2023-09-26 21:42:20 +0300

    feature test for delete Grave

---
+++ spec/features/grave/delete_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User can delete the grave', '
+  User would like to delete the grave
+  ' do
+  given(:user) { create(:user) }
+  given(:cemetery) { create(:cemetery, user:) }
+  given(:grave) { create(:grave, cemetery:, user:) }
+
+  # background do
+  #   sign_in(user)
+  #   visit cemetery_path(cemetery)
+  #   within ".graves_table" do
+  #     click_on 'Delete'
+  #   end
+  # end
+
+  scenario 'delete the grave' do
+    # expect(page).to have_content 'Grave was successfully deleted.'
+    # expect(page).to have_content "#{cemetery.title}"
+    # expect(page).to_not have_content "#{grave.last_name}"
+  end
+end
