5c63840abfea5bea7c727eb81f1f92edd5d5a360
Maksim Korelskii <mkorelskii@gmail.com>
2023-09-26 11:54:46 +0300

    feature test create grave

---
+++ spec/features/grave/create_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'User can create the grave', %q{
+  User would like to create the grave
+} do
+  given(:user) { create(:user) }
+  given(:cemetery) { create(:cemetery, user: user) }
+
+  background do
+    sign_in(user)
+    visit cemetery_path(cemetery)
+  end
+
+  scenario 'create the grave with valid data' do
+    fill_in 'Last name', with: 'Smith'
+    click_on 'Submit Grave!'
+
+    expect(page).to have_content 'Grave was successfully created.'
+    expect(page).to have_content 'Smith'
+  end
+
+  scenario 'create the grave with invalid data' do
+    fill_in 'Last name', with: ''
+    click_on 'Submit Grave!'
+
+    expect(page).to have_content "Last name can't be blank"
+  end
+end
