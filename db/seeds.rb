18c48180062d765009e92af11053f8cb2e1da9db
Maksim Korelskii <mkorelskii@gmail.com>
2023-10-10 15:06:51 +0300

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user = User.create(email: 'demiurge@example.com',
-                   password: 'demiurge@example.com',
-                   password_confirmation: 'demiurge@example.com')
+15.times do
+  name = Faker::Lorem.word.capitalize
+  email = "#{Faker::Lorem.word.capitalize}@example.com"
+  User.create(email: email,
+              name: name,
+              password: email,
+              password_confirmation: email)
+end
 
 10.times do
   title = "Cemetery #{Faker::Lorem.word.capitalize}"
   description = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
-  cemetery = Cemetery.create(title:, description:, user_id: user.id)
+  cemetery = Cemetery.create(title:, description:, user_id: User.last.id)
   rand(20...65).times do
     last_name = Faker::Name.last_name
     first_name = Faker::Name.first_name
@@ -22,6 +27,6 @@ user = User.create(email: 'demiurge@example.com',
     birthday = Faker::Date.birthday(min_age: 18, max_age: 65)
     deathday = Faker::Date.in_date_period
     cemetery.graves.create(last_name:, first_name:, father_name:,
-                           birthday:, deathday:, user_id: user.id)
+                           birthday:, deathday:, user_id: User.last.id)
   end
 end
